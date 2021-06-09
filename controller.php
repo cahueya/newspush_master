a09377ff3306e71af8f9bbb8bf942f6b51e7ac66
cahueya <cahueya@users.noreply.github.com>
2021-06-09 12:20:56 +0200

    Update controller.php

--- controller.php
+++ controller.php
@@ -5,6 +5,7 @@ use Core;
 use Events;
 use Package;
 use SinglePage;
+use Database;
 use Concrete\Core\Asset\Asset;
 use Concrete\Core\Asset\AssetList;
 use Concrete\Core\Support\Facade\Application;
@@ -13,13 +14,23 @@ use Concrete\Core\Entity\Attribute\Key\PageKey;
 use Concrete\Core\Attribute\Key\Category as AttributeKeyCategory;
 use Concrete\Core\Attribute\Type as AttributeType;
 use Concrete\Core\Attribute\Set as AttributeSet;
-
+use Concrete\Core\Page\Type\Type as PageType;
+use PageTemplate;
+use Concrete\Package\NewspushMaster\Theme\NewspusherTheme\PageTheme;
+use \Concrete\Core\Page\Type\PublishTarget\Type\Type as PublishTargetType;
+use \Concrete\Core\Attribute\Key\CollectionKey as CollectionKey;
+use \Concrete\Core\Page\Type\Composer\LayoutSet as LayoutSet;
+use \Concrete\Core\Page\Type\Composer\Control\CorePageProperty\NameCorePageProperty as NameControl;
+use \Concrete\Core\Page\Type\Composer\Control\BlockControl as BlockControl;
+use \Concrete\Core\Page\Type\Composer\Control\CollectionAttributeControl as AttributeControl;
+use BlockType;
+use Concrete\Core\Backup\ContentImporter\Importer\Routine\ImportPageTypesBaseRoutine as CoreImportPageTypesBaseRoutine;
+use Concrete\Package\NewspushMaster\Backup\ContentImporter\Importer\Routine\ImportPageTypesBaseRoutine;
 
 class Controller extends Package {
 	protected $pkgHandle = 'newspush_master';
 	protected $appVersionRequired = '8.4';
-	protected $pkgVersion = '0.0.35';
-
+	protected $pkgVersion = '0.0.97';
     protected $pkgAutoloaderRegistries = [
         'src/NewspushMaster' => '\Concrete\Package\NewspushMaster',
     ];
@@ -35,80 +46,99 @@ class Controller extends Package {
 	public function install () {
 
 		$pkg = parent::install();
+
+        $theme = PageTheme::add('newspusher_theme', $pkg);
+        $theme->applyToSite();
+        $this->app->bind(CoreImportPageTypesBaseRoutine::class, ImportPageTypesBaseRoutine::class);
+
+        $this->installXml();
+        
         SinglePage::add('/dashboard/pages/push',$pkg);
+        \Concrete\Core\Job\Job::installByPackage('newspusher_job', $pkg);
 
         $service = $this->app->make('Concrete\Core\Attribute\Category\CategoryService');
         $categoryEntity = $service->getByHandle('collection');
         $category = $categoryEntity->getController();
-
+        
         $key = $category->getByHandle('push_to_news');
         if (!is_object($key)) {
             $key = new PageKey();
             $key->setAttributeKeyHandle('push_to_news');
             $key->setAttributeKeyName('Push this Page to News');
             $key = $category->add('boolean', $key, null, $pkg);
-        }		
-
-
-        //user attributes for customers
-        $uakc = AttributeKeyCategory::getByHandle('user');
-        $uakc->setAllowAttributeSets(AttributeKeyCategory::ASET_ALLOW_SINGLE);
-
-        //define attr group, and the different attribute types we'll use
-        $custSet = AttributeSet::getByHandle('newspusher_data');
-        if (!is_object($custSet)) {
-            $custSet = $uakc->addSet('newspusher_data', t('Newspusher Data'), $pkg);
+            $key = $category->getByHandle('push_to_news');
+        }	
+        $key = $category->getByHandle('push_date');
+        if (!is_object($key)) {
+            $key = new PageKey();
+            $key->setAttributeKeyHandle('push_date');
+            $key->setAttributeKeyName('Date of the Newspusher Process');
+            $key = $category->add('text', $key, null, $pkg);
+            $key = $category->getByHandle('push_date');
         }
+        $key = $category->getByHandle('push_status_telegram');
+        if (!is_object($key)) {
+            $key = new PageKey();
+            $key->setAttributeKeyHandle('push_status_telegram');
+            $key->setAttributeKeyName('Pushed to Telegram');
+            $key = $category->add('text', $key, null, $pkg);
+            $key = $category->getByHandle('push_status_telegram');
+        } 
+        $key = $category->getByHandle('push_status_twitter');
+        if (!is_object($key)) {
+            $key = new PageKey();
+            $key->setAttributeKeyHandle('push_status_twitter');
+            $key->setAttributeKeyName('Pushed to Twitter');
+            $key = $category->add('text', $key, null, $pkg);
+            $key = $category->getByHandle('push_status_twitter');
+        } 
+        $key = $category->getByHandle('push_status_facebook');
+        if (!is_object($key)) {
+            $key = new PageKey();
+            $key->setAttributeKeyHandle('push_status_facebook');
+            $key->setAttributeKeyName('Pushed to Facebook');
+            $key = $category->add('text', $key, null, $pkg);
+            $key = $category->getByHandle('push_status_facebook');
+        } 
+        $key = $category->getByHandle('push_status_rest');
+        if (!is_object($key)) {
+            $key = new PageKey();
+            $key->setAttributeKeyHandle('push_status_rest');
+            $key->setAttributeKeyName('Pushed to Rest API');
+            $key = $category->add('text', $key, null, $pkg);
+            $key = $category->getByHandle('push_status_rest');
+        } 
+        $key = $category->getByHandle('push_status_sendy');
+        if (!is_object($key)) {
+            $key = new PageKey();
+            $key->setAttributeKeyHandle('push_status_sendy');
+            $key->setAttributeKeyName('Pushed to Sendy');
+            $key = $category->add('text', $key, null, $pkg);
+            $key = $category->getByHandle('push_status_sendy');
+        }   
+        
+        self::setPageTypeDefaults();
 
-        $text = AttributeType::getByHandle('text');
-        $boolean = AttributeType::getByHandle('boolean');
-
-        self::installUserAttribute('user_apiURL', $text, $pkg, $custSet);
-        self::installUserAttribute('user_api_pagepath', $text, $pkg, $custSet);
-        self::installUserAttribute('user_rest_client_id', $text, $pkg, $custSet);
-        self::installUserAttribute('user_rest_client_secret', $text, $pkg, $custSet);
-        self::installUserAttribute('user_tw_consumerKey', $text, $pkg, $custSet);
-        self::installUserAttribute('user_tw_consumerSecret', $text, $pkg, $custSet);
-        self::installUserAttribute('user_tw_accessToken', $text, $pkg, $custSet);
-        self::installUserAttribute('user_tw_accessTokenSecret', $text, $pkg, $custSet);
-        self::installUserAttribute('user_telegramBotToken', $text, $pkg, $custSet);
-        self::installUserAttribute('user_telegramChatID', $text, $pkg, $custSet);
-        self::installUserAttribute('user_li_app_secret', $text, $pkg, $custSet);
-        self::installUserAttribute('user_li_app_id', $text, $pkg, $custSet);
-        self::installUserAttribute('user_fb_app_id', $text, $pkg, $custSet);
-        self::installUserAttribute('user_fb_app_secret', $text, $pkg, $custSet);
-        self::installUserAttribute('user_fb_app_url', $text, $pkg, $custSet);
-        self::installUserAttribute('user_fb_app_pageid', $text, $pkg, $custSet);
-        self::installUserAttribute('user_fb_app_token', $text, $pkg, $custSet);
-        self::installUserAttribute('user_fb_app_long_token', $text, $pkg, $custSet);
-        self::installUserAttribute('user_fb_app_long_page_token', $text, $pkg, $custSet);
-        self::installUserAttribute('user_fb_app_long_page_token_expiry', $text, $pkg, $custSet);
-        self::installUserAttribute('activate_rest_api', $boolean, $pkg, $custSet);
-        self::installUserAttribute('activate_twitter', $boolean, $pkg, $custSet);
-        self::installUserAttribute('activate_telegram', $boolean, $pkg, $custSet);
-        self::installUserAttribute('activate_facebook', $boolean, $pkg, $custSet);
+        
 
     }
 
-    public static function installUserAttribute($handle, $type, $pkg, $set, $data = null)
+
+    public function setPageTypeDefaults()
     {
-        $app = Application::getFacadeApplication();
-        $service = $app->make('Concrete\Core\Attribute\Category\CategoryService');
-        $categoryEntity = $service->getByHandle('user');
+        $service = $this->app->make('Concrete\Core\Attribute\Category\CategoryService');
+        $categoryEntity = $service->getByHandle('collection');
         $category = $categoryEntity->getController();
+        $key = $category->getByHandle('push_to_news');
+        $pageType = PageType::getByHandle('blog_entry');
+        $template = $pageType->getPageTypeDefaultPageTemplateObject();
+        $pageObj = $pageType->getPageTypePageTemplateDefaultPageObject($template);
+        //$pageObj->setAttribute($key,1);
+    }
 
-        $attr = $category->getByHandle($handle);
-
-        if (!is_object($attr)) {
-            $name = Application::getFacadeApplication()->make("helper/text")->unhandle($handle);
-
-            $key = new UserKey();
-            $key->setAttributeKeyHandle($handle);
-            $key->setAttributeKeyName(t($name));
-            $key = $category->add($type, $key, null, $pkg);
-
-            $key->setAttributeSet($set);
-        }
+    private function installXml()
+    {
+        $this->installContentFile('install.xml');
     }
 
     public function uninstall() {
@@ -123,16 +153,10 @@ class Controller extends Package {
     public function on_start() {   
         require $this->getPackagePath() . '/vendor/autoload.php';
         $event = Core::make('Concrete\Package\NewspushMaster\Push');
-        Events::addListener('on_page_type_publish', array($event, 'pageAdd'));
+        Events::addListener('on_page_version_approve', array($event, 'pageAdd'));
 
         $al = AssetList::getInstance();
         $al->register('css', 'push', 'css/push.css?v=' . $version, ['version' => $version, 'position' => Asset::ASSET_POSITION_HEADER, 'minify' => false, 'combine' => false], $this);
         $al->register('javascript', 'push', 'js/push.js?v=' . $version, ['version' => $version, 'position' => Asset::ASSET_POSITION_FOOTER, 'minify' => false, 'combine' => false], $this);
-
-
-
-
-
-
     }
 }
